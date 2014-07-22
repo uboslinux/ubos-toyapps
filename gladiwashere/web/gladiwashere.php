d8c6237aa097b28611db1736685081aa78466c2e
Johannes Ernst <jernst@indiebox-dev.aviatis.com>
2014-07-22 04:11:28 +0000

    initial checkin

---
+++ gladiwashere/web/gladiwashere.php
@@ -0,0 +1,92 @@
+<?php
+#
+# Program code for gladiwashere.
+#
+# This file is part of gladiwashere.
+# (C) 2012-2014 Indie Computing Corp.
+#
+# gladiwashere is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# gladiwashere is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with gladiwashere.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+    $db = mysql_connect( $dbServer, $dbUser, $dbPass );
+    if( !$db ) {
+        die( 'ERROR: Cannot connect: ' . mysql_error() );
+    }
+    if( !mysql_select_db( $dbName, $db )) {
+        die( 'Error: Cannot select database ' . $dbName );
+    }
+    if( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
+        mysql_query(
+                sprintf(
+                        "INSERT INTO Comment( name, created, email, comment ) VALUES ( '%s', NOW(), '%s', '%s' );",
+                        mysql_real_escape_string( $_POST['name'] ),
+                        mysql_real_escape_string( $_POST['email'] ),
+                        mysql_real_escape_string( $_POST['comment'] )),
+                $db );
+    }
+    $query = mysql_query( "SELECT * FROM Comment ORDER BY id DESC LIMIT 10", $db );
+
+?>
+<html>
+ <head>
+  <title>Glad-I-Was-Here Guestbook</title>
+ </head>
+ <body>
+  <h1>Glad-I-Was-Here Guestbook</h1>
+  <p>Example MySQL/PHP app for <a href="http://indieboxproject.org/">Indie Box</a>.</p>
+<?php
+$first = TRUE;
+while( $row = mysql_fetch_assoc( $query )) {
+    if( $first ) {
+        echo "<h2>Comments:</h2>\n";
+        echo "<dl>\n";
+    }
+    echo " <dt>" . $row['name'] . " (on " . $row['created'] . ")</dt>\n";
+    echo " <dd>" . $row['comment'] . "</dd>\n";
+    $first = FALSE;
+}
+if( !$first ) {
+    echo "</dl>\n";
+}
+mysql_close( $db );
+?>
+  <p>Please leave your comments here:</p>
+  <form method="POST" action="<?php $_SERVER['PHP_SELF'] ?>">
+   <table>
+    <tr>
+     <th>Name:</th>
+     <td>
+      <input type="text" name="name" size="40" maxlength="80" />
+     </td>
+    </tr>
+    <tr>
+     <th>E-mail:</th>
+     <td>
+      <input type="text" name="email" size="40" maxlength="80" />
+     </td>
+    </tr>
+    <tr>
+     <th>Comment:</th>
+     <td>
+      <textarea name="comment" maxlength="1024" cols="40" rows="8" ></textarea>
+     </td>
+    </tr>
+    <tr>
+     <td colspan="2" class="submit">
+      <input type="submit" name="submit" value="Leave Comment" />
+     </td>
+   </table>
+  </form>
+ </body>
+</html>
